5bbe0f82adc7a59e3c4cb8d9d2b794c9dd4f4550
Nathan <nathan_heard@ymail.com>
2023-08-28 12:05:05 +0100

    update script added

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# fetch .ohmyzsh update from remote repo
+	# this is a submodle that has been forked so might need to figure that out too
+# pull Spacevim update
+# pull update for this repo
